d787001a3f19505b704a20fc1328089c5aa2aad6
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-21 15:56:59 -0300

    AP1 Concluída

--- ed2_ap1.dart
+++ ed2_ap1.dart
@@ -1,1 +1,11 @@
-void main() {}
+import 'dart:math';
+
+void main() {
+  final aleatorio = Random();
+
+  final lista = List.generate(10, (_) => aleatorio.nextInt(100));
+
+  lista.forEach((numero) {
+    print("Posição: ${lista.indexOf(numero)} // Valor: $numero");
+  });
+}
