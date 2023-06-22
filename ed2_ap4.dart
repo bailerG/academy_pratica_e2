46f676ff6acf84321d39bdc6fb60b08a2b574b08
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-06-22 07:38:51 -0300

    AP4 Concluida

--- ed2_ap4.dart
+++ ed2_ap4.dart
@@ -6,11 +6,27 @@ void main() {
     'MG': ['Belo Horizonte', 'Juiz de Fora', 'Berlinda'],
   };
 
-  print("Estados: ${estados.keys.join("; ")}");
+  // print("Estados: ${estados.keys.join("; ")}");
 
   final santaCatarina = estados['SC']!;
 
   santaCatarina.sort();
 
-  print("Cidades de SC: $santaCatarina");
+  // print("Cidades de SC: ${santaCatarina.join(" ; ")}");
+
+  final cidadesFormatadas = [];
+
+  for (var sigla in estados.keys) {
+    final cidadesDesseEstado = estados[sigla]!;
+
+    for (var cidade in cidadesDesseEstado) {
+      cidadesFormatadas.add('$cidade - $sigla');
+    }
+  }
+
+  cidadesFormatadas.sort();
+
+  for (var cidade in cidadesFormatadas) {
+    print(cidade);
+  }
 }
